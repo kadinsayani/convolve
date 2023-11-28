1548a39a4c2d088f55cca9ef34037cc2244be8af
Kadin Sayani <me@kadinsayani.dev>
2023-11-28 14:40:27 -0700

    added initial tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -13,3 +13,15 @@ TEST_CASE("Convolve Function Test", "[convolve]") {
   std::vector<float> result = convolver.convolve(x, h);
   REQUIRE(result == std::vector<float>{0.5, 1.5, 2.5, 1.5});
 }
+
+TEST_CASE("WavFile read", "[wavfile]") {
+  WavFile wavFile;
+  REQUIRE_NOTHROW(wavFile.read("guitar_short.wav"));
+  SECTION("Check audio data") { REQUIRE(wavFile.audioData.size() > 0); }
+}
+
+TEST_CASE("WavFile write", "[wavfile]") {
+  WavFile wavFile;
+  wavFile.read("guitar_short.wav");
+  REQUIRE_NOTHROW(wavFile.write("output.wav"));
+}
