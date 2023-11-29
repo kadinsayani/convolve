987697cd76993641db82672af0d43e399a15965e
Kadin Sayani <me@kadinsayani.dev>
2023-11-29 11:41:50 -0700

    added fft

--- src/Convolver.cpp
+++ src/Convolver.cpp
@@ -2,22 +2,81 @@
 
 using namespace Convolve;
 
+const double pi = acos(-1);
+#define SWAP(a, b)                                                             \
+  tempr = (a);                                                                 \
+  (a) = (b);                                                                   \
+  (b) = tempr
+
 std::vector<float> Convolver::convolve(const std::vector<float> &x,
                                        const std::vector<float> &h) {
   std::vector<float> y(x.size() + h.size() - 1, 0.0);
   std::cout << "Convolving input signal and impulse response" << std::endl;
-
-  int n, m;
-
-  for (n = 0; n < y.size(); n++) {
-    y[n] = 0.0;
-  }
-
-  for (n = 0; n < x.size(); n++) {
-    for (m = 0; m < h.size(); m++) {
-      y[n + m] += x[n] * h[m];
-    }
-  }
-
-  return y;
 }
+
+// std::vector<float> Convolver::convolve(const std::vector<float> &x,
+//                                        const std::vector<float> &h) {
+//   std::vector<float> y(x.size() + h.size() - 1, 0.0);
+//   std::cout << "Convolving input signal and impulse response" << std::endl;
+
+//   int n, m;
+
+//   for (n = 0; n < y.size(); n++) {
+//     y[n] = 0.0;
+//   }
+
+//   for (n = 0; n < x.size(); n++) {
+//     for (m = 0; m < h.size(); m++) {
+//       y[n + m] += x[n] * h[m];
+//     }
+//   }
+
+//   return y;
+// }
+
+void Convolver::fft(float data[], int nn, int isign) {
+  unsigned long n, mmax, m, j, istep, i;
+  double wtemp, wr, wpr, wpi, wi, theta;
+  float tempr, tempi;
+
+  n = nn << 1;
+  j = 1;
+
+  for (i = 1; i < n; i += 2) {
+    if (j > i) {
+      SWAP(data[j], data[i]);
+      SWAP(data[j + 1], data[i + 1]);
+    }
+    m = nn;
+    while (m >= 2 && j > m) {
+      j -= m;
+      m >>= 1;
+    }
+    j += m;
+  }
+
+  mmax = 2;
+  while (n > mmax) {
+    istep = mmax << 1;
+    theta = isign * (6.28318530717959 / mmax);
+    wtemp = sin(0.5 * theta);
+    wpr = -2.0 * wtemp * wtemp;
+    wpi = sin(theta);
+    wr = 1.0;
+    wi = 0.0;
+    for (m = 1; m < mmax; m += 2) {
+      for (i = m; i <= n; i += istep) {
+        j = i + mmax;
+        tempr = wr * data[j] - wi * data[j + 1];
+        tempi = wr * data[j + 1] + wi * data[j];
+        data[j] = data[i] - tempr;
+        data[j + 1] = data[i + 1] - tempi;
+        data[i] += tempr;
+        data[i + 1] += tempi;
+      }
+      wr = (wtemp = wr) * wpr - wi * wpi + wr;
+      wi = wi * wpr + wtemp * wpi + wi;
+    }
+    mmax = istep;
+  }
+};
--- src/Convolver.h
+++ src/Convolver.h
@@ -6,5 +6,6 @@ class Convolver {
 public:
   std::vector<float> convolve(const std::vector<float> &x,
                               const std::vector<float> &h);
+  void fft(float data[], int nn, int isign);
 };
 } // namespace Convolve
